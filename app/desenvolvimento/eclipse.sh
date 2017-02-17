9ff477d3cf3498fe606fc48a5acafd479820fe60
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-02-16 22:17:10 -0200

    Adicionando script de instalação do Eclipse

---
+++ app/desenvolvimento/eclipse.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+wget http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/neon/2/eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz -O /home/matheush/Downloads/eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz
+
+cd /home/matheush/Downloads
+
+sudo tar xvzf ./eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz -C ./
+
+sudo mv ./eclipse /opt/
+
+sudo touch /usr/share/applications/eclipse.desktop
+ 
+sudo chmod 666 /usr/share/applications/eclipse.desktop
+ 
+sudo echo "[Desktop Entry]" > /usr/share/applications/eclipse.desktop
+sudo echo "Version=1.0" >> /usr/share/applications/eclipse.desktop
+sudo echo "Type=Application" >> /usr/share/applications/eclipse.desktop
+sudo echo "Terminal=false" >> /usr/share/applications/eclipse.desktop
+sudo echo "Name=Eclipse" >> /usr/share/applications/eclipse.desktop
+sudo echo "Exec=/opt/eclipse/eclipse" >> /usr/share/applications/eclipse.desktop
+sudo echo "Icon=/opt/eclipse/icon.xpm" >> /usr/share/applications/eclipse.desktop
+sudo echo "Categories=Application;Development;Java;IDE" >> /usr/share/applications/eclipse.desktop
+ 
+sudo chmod 644 /usr/share/applications/eclipse.desktop
