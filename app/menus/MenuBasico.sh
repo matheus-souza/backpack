8c598e90065e6d7bc1186df953976fde629855ac
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-02-20 20:41:43 -0300

    Refatorando pastas Desenvolvedor

--- app/menus/MenuBasico.sh
+++ app/menus/MenuBasico.sh
@@ -3,3 +3,29 @@
 senha=0
 usuario=0
 
+#Gimp() {}
+
+#Gksu() {}
+
+#Inkscape() {}
+
+#RarUnrar() {}
+
+#RestrictedExtras() {}
+
+#UnityTweakTool() {}
+
+InstalaTudo() {
+	Gimp
+	Gksu
+	Inkscape
+	RarUnrar
+	RestrictedExtras
+	UnityTweakTool
+}
+
+echo "antes do em branco"
+
+./app/menus/complementares/EmBranco.sh
+
+echo "passou"
