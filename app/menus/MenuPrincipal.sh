fde2c23923b340efb60ca4c29a4560c836e7896f
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-02-20 21:14:51 -0300

    Começando a modelar menu principal

--- app/menus/MenuPrincipal.sh
+++ app/menus/MenuPrincipal.sh
@@ -3,27 +3,48 @@
 senha=0
 usuario=0
 
-#direto=0
-#direto=$(pwd)
-#echo $direto
+direto=0
+direto=$(pwd)
+echo $direto
 
 senha=$(cat \./\.tmp/\.senha)
-echo $senha
+#echo $senha
 
 usuario=$(cat \./\.tmp/\.usuario)
-echo $usuario
+#echo $usuario
+
+MenuAtualizador() {
+	./app/complementares/atualiza.sh
+}
+
+#MenuLimpaSistema() {
+	#CHAMA LIMPA SISEMA
+#}
+
+MenuBasico() {
+	./app/menus/MenuBasico.sh
+}
+
+MenuDesenvolvedor() {
+	./app/menus/MenuDesenvolvedor.sh
+}
+
+MenuAdicionais() {
+	./app/menus/MenuAdicionais.sh
+}
+
 
 itemSelecionado=$(whiptail --title "Menu scripts-ubuntu" --menu "Selecione uma das opções" 20 78 7 \
 	"1" "Atualizar lista de repositórios e atualizar programas" \
-	"2" "Pacote básico" \
-	"3" "Pacote de desenvolvedor" \
-	"4" "Pacote Adicionais" \
-	"5" "Quem faz" 3>&1 1>&2 2>&3)
+	"2" "" \
+	"3" "Pacote básico" \
+	"4" "" \
+	"5" "Pacote Adicionais" 3>&1 1>&2 2>&3)
 	
 case $itemSelecionado in
-	1) ChamarAtualizador ;;
-	2) ChamarLimpaSistema ;;
-	3) CahamarMenuBasico ;;
-	4) ChamarMenuDesenvolvedor ;;
-	5) CahamarMenuAdicionais ;;
+	1) MenuAtualizador ;;
+	2) MenuLimpaSistema ;;
+	3) MenuBasico ;;
+	4) MenuDesenvolvedor ;;
+	5) MenuAdicionais ;;
 esac
