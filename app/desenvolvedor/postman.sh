3a000bd5c6f671740778c7b81cca15cbf0ccf6c1
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-03-31 00:24:31 -0300

    Realizando ajuste no script de instalação do Postman

--- app/desenvolvedor/postman.sh
+++ app/desenvolvedor/postman.sh
@@ -16,7 +16,7 @@ sudo echo "[Desktop Entry]" > /usr/share/applications/postman.desktop
 sudo echo "Version=1.0" >> /usr/share/applications/postman.desktop
 sudo echo "Type=Application" >> /usr/share/applications/postman.desktop
 sudo echo "Terminal=false" >> /usr/share/applications/postman.desktop
-sudo echo "Name=postman" >> /usr/share/applications/postman.desktop
+sudo echo "Name=Postman" >> /usr/share/applications/postman.desktop
 sudo echo "Exec=/opt/Postman/Postman" >> /usr/share/applications/postman.desktop
 sudo echo "Icon=/opt/Postman/postman.png" >> /usr/share/applications/postman.desktop
 sudo echo "Categories=Application;" >> /usr/share/applications/postman.desktop
@@ -26,3 +26,5 @@ sudo chmod 644 /usr/share/applications/postman.desktop
 wget https://www.dropbox.com/s/q1aycrntwlmv4w8/postman.png?dl=0 -O ~/Downloads/postman.png
  
 sudo mv /home/matheush/Downloads/postman.png /opt/Postman/
+
+sudo rm /home/matheush/Downloads/Postman.tar.gz
