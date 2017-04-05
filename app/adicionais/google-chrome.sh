53058dad73d2bcad6a9a1cae4a5c030080fe39c7
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-04 23:52:17 -0300

    Atualizando script de instalação do Google Chrome

--- app/adicionais/google-chrome.sh
+++ app/adicionais/google-chrome.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /home/matheush/Downloads/google-chrome-stable_current_amd64.deb
+usuario=0
 
-sudo dpkg -i /home/matheush/Downloads/google-chrome-stable_current_amd64.deb
+usuario=$(cat \./\.tmp/\.usuario)
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /home/$usuario/Downloads/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i /home/$usuario/Downloads/google-chrome-stable_current_amd64.deb
 
 sudo apt-get -f install -y
 
-sudo rm /home/matheush/Downloads/google-chrome-stable_current_amd64.deb
+sudo rm /home/$usuario/Downloads/google-chrome-stable_current_amd64.deb
