36734d8187dedded7935621a54b9e8749be17b8c
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-04 23:52:46 -0300

    Atualizando script de instalação do Spotify

--- app/adicionais/spotify.sh
+++ app/adicionais/spotify.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.49.125.g72ee7853-83_amd64.deb -O ~/Downloads/spotify-client_1.0.49.125.g72ee7853-83_amd64.deb
+usuario=0
 
-sudo dpkg -i ~/Downloads/spotify-client_1.0.49.125.g72ee7853-83_amd64.deb
+usuario=$(cat \./\.tmp/\.usuario)
+
+wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.49.125.g72ee7853-83_amd64.deb -O /home/$usuario/Downloads/spotify-client_1.0.49.125.g72ee7853-83_amd64.deb
+
+sudo dpkg -i /home/$usuario/Downloads/spotify-client_1.0.49.125.g72ee7853-83_amd64.deb
 
 sudo apt-get -f install
 
-sudo rm ~/Downloads/spotify-client_1.0.49.125.g72ee7853-83_amd64.deb
+sudo rm /home/$usuario/Downloads/spotify-client_1.0.49.125.g72ee7853-83_amd64.deb
