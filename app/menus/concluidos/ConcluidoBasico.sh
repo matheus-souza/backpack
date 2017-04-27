647691b962a4a3a6ee96f1d632c661f59af7e666
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-27 00:55:50 -0300

    Commit de ajuste

--- app/menus/concluidos/ConcluidoBasico.sh
+++ app/menus/concluidos/ConcluidoBasico.sh
@@ -6,7 +6,6 @@ MenuBasico() {
 
 ConcluidoBasico(){
 	whiptail --title "scripts-ubuntu" --msgbox "A operação desejada acaba de ser concluída, pressione enter para prosseguir." 8 78
-	MenuBasico
 }
 
 ConcluidoBasico
