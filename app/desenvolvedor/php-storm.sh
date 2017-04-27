b274895fa198e39b1a2383ed8eda2ec7ecd6de56
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-27 12:43:21 -0300

    Adicionado script de instalação para PHPStorm

---
+++ app/desenvolvedor/php-storm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+usuario=$(cat \./\.tmp/\.usuario)
+
+wget https://download-cf.jetbrains.com/webide/PhpStorm-2017.1.2.tar.gz -O /home/$usuario/Downloads/PhpStorm-2017.1.2.tar.gz
+
+sudo tar xf /home/$usuario/Downloads/PhpStorm-2017.1.2.tar.gz -C /opt/
+
+cd /opt/PhpStorm-171.4249.3/bin/
+
+sudo chmod +x ./phpstorm.sh
+
+sudo ./phpstorm.sh
+
+sudo chmod 777 /opt/PhpStorm-171.4249.3 -R
