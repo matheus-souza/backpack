fa448ab09ac487a32677db829d578209b0094530
Tiago Portaluppi <tiagoportaluppi.tp@gmail.com>
2017-04-26 21:49:06 -0300

    Update google-chrome.sh

--- app/adicionais/google-chrome.sh
+++ app/adicionais/google-chrome.sh
@@ -6,6 +6,6 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -
 
 sudo dpkg -i /home/$usuario/Downloads/google-chrome-stable_current_amd64.deb
 
-sudo apt-get -f install -y
+sudo apt -f install -y
 
 sudo rm /home/$usuario/Downloads/google-chrome-stable_current_amd64.deb
