c3116931ad7683e2af82965487c3c95a93de8f54
Tiago Portaluppi <tiagoportaluppi.tp@gmail.com>
2017-04-26 21:53:47 -0300

    Update java-jdk.sh

--- app/desenvolvedor/java-jdk.sh
+++ app/desenvolvedor/java-jdk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt-get purge openjdk* -y
+sudo apt purge openjdk* -y
 
 sudo add-apt-repository ppa:webupd8team/java -y
 
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install oracle-java8-installer -y
+sudo apt install oracle-java8-installer -y
 
-sudo apt-get install oracle-java8-set-default -y
+sudo apt install oracle-java8-set-default -y
