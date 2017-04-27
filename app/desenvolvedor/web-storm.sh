819a8588554c7dc2cbd19792e97727a6d10a612f
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-27 12:48:01 -0300

    Ajuste no script do WebStorm

--- app/desenvolvedor/web-storm.sh
+++ app/desenvolvedor/web-storm.sh
@@ -5,7 +5,11 @@ usuario=$(cat \./\.tmp/\.usuario)
 wget https://download-cf.jetbrains.com/webstorm/WebStorm-2017.1.1.tar.gz -O /home/$usuario/Downloads/WebStorm-2017.1.1.tar.gz
 
 sudo tar xf /home/$usuario/Downloads/WebStorm-2017.1.1.tar.gz -C /opt/
+
 cd /opt/WebStorm-171.4073.40/bin/
-sudo chmod +x ./webstorm.sh 
-sudo ./webstorm.sh 
+
+sudo chmod +x ./webstorm.sh
+
+sudo ./webstorm.sh
+
 sudo chmod 777 /opt/WebStorm-171.4073.40 -R
