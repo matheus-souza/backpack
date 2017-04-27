3fef8033d154ed6c97279a34c4b12b745790d2dc
Tiago Portaluppi <tiagoportaluppi.tp@gmail.com>
2017-04-26 21:57:56 -0300

    Update node-js.sh

--- app/desenvolvedor/node-js.sh
+++ app/desenvolvedor/node-js.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt install -y nodejs
 
-sudo apt-get install -y build-essential
+sudo apt install -y build-essential
+
+sudo apt install npm -y
