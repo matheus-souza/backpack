f379960360e7f2f6b71aa50bd54f702cfaf31090
Tiago Portaluppi <tiagoportaluppi.tp@gmail.com>
2017-04-26 22:01:22 -0300

    Update scenebuilder.sh

--- app/desenvolvedor/scenebuilder.sh
+++ app/desenvolvedor/scenebuilder.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-wget http://download.gluonhq.com/scenebuilder/8.3.0/install/linux/scenebuilder-8.3.0.deb -O /home/matheush/Downloads/scenebuilder-8.3.0.deb
+usuario=$(cat \./\.tmp/\.usuario)
 
-sudo dpkg -i /home/matheush/Downloads/scenebuilder-8.3.0.deb
+wget http://download.gluonhq.com/scenebuilder/8.3.0/install/linux/scenebuilder-8.3.0.deb -O /home/$usuario/Downloads/scenebuilder-8.3.0.deb
 
-sudo apt-get -f install -y
+sudo dpkg -i /home/$usuario/Downloads/scenebuilder-8.3.0.deb
+
+sudo apt -f install -y
 
 sudo rm /home/matheush/Downloads/scenebuilder-8.3.0.deb
