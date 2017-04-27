9a455af90e48ef74296c6717620c52dd804db8c1
Tiago Portaluppi <tiagoportaluppi.tp@gmail.com>
2017-04-26 22:03:01 -0300

    Update web-storm.sh

--- app/desenvolvedor/web-storm.sh
+++ app/desenvolvedor/web-storm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo tar xf /home/matheush/Downloads/WebStorm-2017.1.1.tar.gz -C /opt/
+usuario=$(cat \./\.tmp/\.usuario)
+
+sudo tar xf /home/$usuario/Downloads/WebStorm-2017.1.1.tar.gz -C /opt/
 cd /opt/WebStorm-171.4073.40/bin/
 sudo chmod +x ./webstorm.sh 
 sudo ./webstorm.sh 
