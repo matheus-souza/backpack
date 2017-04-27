c1a9ae53120a5d5452dced0314510662a303cd18
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-27 02:23:31 -0300

    Implementando menu adicionais

--- app/menus/Adicionais.sh
+++ app/menus/Adicionais.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-senha=0
-usuario=0
-
 senha=$(cat \./\.tmp/\.senha)
 usuario=$(cat \./\.tmp/\.usuario)
 
-MenuConcluidoAdicionais() {
+Concluido() {
 	./app/menus/complementares/Concluido.sh
 }
 
-#Franz() {}
+Franz() {
+	echo -e "$senha\n" | sudo -S ./app/adicionais/franz.sh
+}
 
-#GoogleChrome() {}
+GoogleChrome() {
+	echo -e "$senha\n" | sudo -S ./app/adicionais/google-chrome.sh
+}
 
-#Kdenlive() {}
+Kdenlive() {
+	echo -e "$senha\n" | sudo -S ./app/adicionais/kdenlive.sh
+}
 
-#SimpleScreenRecorder() {}
+SimpleScreenRecorder() {
+	echo -e "$senha\n" | sudo -S ./app/adicionais/simple-recorder.sh
+}
 
-#Spotify() {}
+Spotify() {
+	echo -e "$senha\n" | sudo -S ./app/adicionais/spotify.sh
+}
 
-#Transmageddon() {}
+Transmageddon() {
+	echo -e "$senha\n" | sudo -S ./app/adicionais/transmageddon.sh
+}
 
-MenuAdicionais() {
+Adicionais() {
 	escolha=$(whiptail --title "Menu Adicionais" --menu "Pacote Adicionais" 20 78 7 \
 	"1" "Instalar todos os programas" \
 	"2" "Escolher o que instalar" \
@@ -60,51 +69,32 @@ EscolherProgramas() {
 	"Spotify" "Player de músicas" OFF \
 	"Transmageddon" "Conversor de videos" OFF 3>&1 1>&2 2>&3)
 
-	statusCheck=$?
-	if [ $statusCheck = 0 ]
-	then
-		echo "$checkBox" | while read LINHA
-		do
-	 		#echo "$LINHA"
-			if [ "$LINHA" = "Franz" ];
-			then
-				Franz
-			fi
-			if [ "$LINHA" = "GoogleChrome" ];
-			then
-				GoogleChrome
-			fi
-			if [ "$LINHA" = "Kdenlive" ];
-			then
-				Kdenlive
-			fi
-			if [ "$LINHA" = "SimpleScreenRecorder" ];
-			then
-				SimpleScreenRecorder
-			fi
-			if [ "$LINHA" = "Spotify" ];
-			then
-				Spotify
-			fi
-			if [ "$LINHA" = "Transmageddon" ];
-			then
-				Transmageddon
-			fi
-		done
-	else
-		EmBranco
-		MenuAdicionais
+	echo "$checkBox" | while read item
+	do
+		case $item in
+			"Franz") Franz ;;
+			"Google Chrome") GoogleChrome ;;
+			"Kdenlive") Kdenlive ;;
+			"Simple Screen Recorder") SimpleScreenRecorder ;;
+			"Spotify") Spotify ;;
+			"Transmageddon") Transmageddon ;;
+			*) EmBranco ;;
+		esac
+	done
+
+	if [[ "$checkBox" != "" ]]; then
+		Concluido
 	fi
-	MenuConcluidoAdicionais
-	MenuAdicionais
+	Adicionais
 }
 
 Detalhes() {
 	./app/menus/detalhes/Adicionais.sh
+	Adicionais
 }
 
 Voltar() {
 	./app/menus/Principal.sh
 }
 
-MenuAdicionais
+Adicionais
