36efb22e09fc8c849b727c5b67fe3527fbaa4998
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-28 13:28:36 -0300

    Iniciando a implementação do menu de desenvolvedor

--- app/menus/Desenvolvedor.sh
+++ app/menus/Desenvolvedor.sh
@@ -1,5 +1,137 @@
 #!/bin/bash
 
-senha=0
-usuario=0
+senha=$(cat \./\.tmp/\.senha)
+usuario=$(cat \./\.tmp/\.usuario)
 
+Concluido() {
+	./app/menus/complementares/Concluido.sh
+}
+
+AdminMongo() {
+	#chamar node
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/admin-mongo.sh
+}
+
+AndrodStudio() {
+	#verifica se java ta instalado e chamar java
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/android-studio.sh
+}
+
+Curl() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/curl.sh
+}
+
+Eclipse() {
+	#verifica se java ta instalado e chamar java
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/eclipse.sh
+}
+
+GitKraken() {
+	#Chamar git
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/git-kraken.sh
+}
+
+Git() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/git.sh
+}
+
+JavaJdk() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/java-jdk.sh
+}
+
+MongoDb() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/mongodb.sh
+}
+
+Netbeans() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/netbeans.sh
+}
+
+NodeJs() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/node-js.sh
+}
+
+Nodemon() {
+	#verificar se node ta instalado e chamar node
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/nodemon.sh
+}
+
+PhpStorm() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/php-storm.sh
+}
+
+Postman() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/postman.sh
+}
+
+Pycharm() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/pycharm.sh
+}
+
+SceneBuilder() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/scenebuilder.sh
+}
+
+SublimeText() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/sublime-text.sh
+}
+
+WebStorm() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/web-storm.sh
+}
+
+Xampp() {
+	echo -e "$senha\n" | sudo -S ./app/desenvolvedor/xampp.sh
+}
+
+Desenvolvedor() {
+	escolha=$(whiptail --title "Menu Básico" --menu "Pacote Básico" 20 78 7 \
+	"1" "Instalar todos os programas" \
+	"2" "Escolher o que instalar" \
+	"3" "O que contém no pacote" \
+	"<-" "Voltar para a tela inicial" 3>&1 1>&2 2>&3)
+		
+	case $escolha in
+		1) InstalaTudo ;;
+		2) EscolherProgramas ;;
+		3) Detalhes ;;
+		"<-") Voltar ;;
+	esac
+}
+
+EmBranco() {
+	./app/menus/complementares/EmBranco.sh
+}
+
+InstalaTudo() {
+}
+
+EscolherProgramas() {
+	checkBox=$(whiptail --title "Backpack" --separate-output --checklist \
+	"Escolha os programas para instalar" 20 78 8 \
+	"PHP Storm" "IDE para PHP" OFF \
+	"Xampp" "Ambiente de desenvolvimento PHP" OFF 3>&1 1>&2 2>&3)
+
+	echo "$checkBox" | while read item
+	do
+		case $item in
+			*) EmBranco ;;
+		esac
+	done
+
+	if [[ "$checkBox" != "" ]]; then
+		Concluido
+	fi
+	Desenvolvedor
+}
+
+Detalhes() {
+	./app/menus/detalhes/Basico.sh
+	Desenvolvedor
+}
+
+Voltar() {
+	./app/menus/Principal.sh
+}
+
+Desenvolvedor
