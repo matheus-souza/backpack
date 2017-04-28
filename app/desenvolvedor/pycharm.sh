e707bc6a82bacaac32cd338fabc31902d4130748
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-28 13:45:35 -0300

    Adicionado script de instalação do PyCharm

---
+++ app/desenvolvedor/pycharm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+usuario=$(cat \./\.tmp/\.usuario)
+
+wget https://download-cf.jetbrains.com/python/pycharm-professional-2017.1.2.tar.gz -O /home/$usuario/Downloads/pycharm-professional-2017.1.2.tar.gz
+
+sudo tar xf /home/$usuario/Downloads/pycharm-professional-2017.1.2.tar.gz -C /opt/
+
+cd /opt/pycharm-2017.1.2/bin/
+
+sudo chmod +x ./pycharm.sh
+
+sudo chmod 777 /opt/pycharm-2017.1.2 -R
+
+./pycharm.sh
