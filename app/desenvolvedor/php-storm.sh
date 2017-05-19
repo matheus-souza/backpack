bb8b57f7085d47d528ddda845b3f3d04e4c7601e
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-05-19 17:47:26 -0300

    Atualizando script de instalador do PHPStorm

--- app/desenvolvedor/php-storm.sh
+++ app/desenvolvedor/php-storm.sh
@@ -2,16 +2,18 @@
 
 usuario=$(cat \./\.tmp/\.usuario)
 
-wget https://download-cf.jetbrains.com/webide/PhpStorm-2017.1.2.tar.gz -O /home/$usuario/Downloads/PhpStorm-2017.1.2.tar.gz
+sudo rm /opt/PhpStorm-171.4249.3 -R
 
-sudo tar xf /home/$usuario/Downloads/PhpStorm-2017.1.2.tar.gz -C /opt/
+wget https://download-cf.jetbrains.com/webide/PhpStorm-2017.1.4.tar.gz -O /home/$usuario/Downloads/PhpStorm-2017.1.4.tar.gz
 
-sudo rm /home/$usuario/Downloads/PhpStorm-2017.1.2.tar.gz
+sudo tar xf /home/$usuario/Downloads/PhpStorm-2017.1.4.tar.gz -C /opt/
 
-cd /opt/PhpStorm-171.4249.3/bin/
+sudo rm /home/$usuario/Downloads/PhpStorm-2017.1.4.tar.gz
+
+cd /opt/PhpStorm-171.4694.2/bin/	
 
 sudo chmod +x ./phpstorm.sh
 
-sudo chmod 777 /opt/PhpStorm-171.4249.3 -R
+sudo chmod 777 /opt/PhpStorm-171.4694.2 -R
 
-./phpstorm.sh
+sudo -H -u $usuario bash -c "./phpstorm.sh"
