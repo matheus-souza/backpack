163e616dd2d30bd1dd21c75c930f5138d17da496
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-06-05 21:25:45 -0300

    Commit de ajuste

---
+++ testes/teste_menu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#dat
+#list
+
+
+variavelTeste=0
+
+TesteFuncao() {
+	./testenovo.sh
+}
+
+
+EscolheBasico(){
+	checkbasico=$(whiptail --title "Backpack" --separate-output --checklist \
+	"Escolha os programas para instalar" 20 78 8 \
+	"teste" "Decodificador de vídeo" OFF 3>&1 1>&2 2>&3)
+
+		echo "$checkbasico" | while read LINHA
+		do
+	 		#echo "$LINHA"
+			if [ "$LINHA" = "teste" ];
+			then
+				
+
+				variavelTeste="testando"
+
+				touch ./.senha.txt
+
+				echo $variavelTeste > .senha.txt
+
+				TesteFuncao
+			fi
+		done
+}
+
+EscolheBasico
+
+echo "voltou"
