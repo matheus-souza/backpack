242a417acf7441e802b9b874086ccf948c6b42a5
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-06-09 01:41:50 -0300

    Ajustanto login

--- run.sh
+++ run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-senha=0
-usuario=0
-
 chmod +x ./ -R
 
-usuario=$(whiptail --title "Backpack" --inputbox "Por favor digite seu usuário" 8 78 3>&1 1>&2 2>&3)
+usuario=$(whiptail --title "Backpack" --inputbox "Por favor digite seu usuário" --nocancel 8 78 3>&1 1>&2 2>&3)
 
-senha=$(whiptail --title "Backpack" --passwordbox "Por favor digite sua senha" 8 78 3>&1 1>&2 2>&3)
+senha=$(whiptail --title "Backpack" --passwordbox "Por favor digite sua senha" --nocancel 8 78 3>&1 1>&2 2>&3)
+
+#Verirfica se foi digitado usuario e senha
+if [[ "$usuario" != "" && "$senha" != "" ]]; then
+	MenuPrincipal
+else
+	whiptail --title "Erro!" --msgbox "Houve um problema ao autenticar o usuário" 8 60
+	exit 0
+fi
+
 
 MenuPrincipal() {
 	./app/menus/Principal.sh
