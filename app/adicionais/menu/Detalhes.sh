ad460c9a76fc654b277400eb4487e03393db96ca
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-06-17 14:35:55 -0300

    Alterando estrutura de diretórios

---
+++ app/adicionais/menu/Detalhes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+Detalhes() {
+	whiptail --title "Detalhes pacote adicionais" --msgbox "
+		Dropbox -> Serviço para armazenamento de arquivos
+		Franz -> Agregador de chats
+		Google Chrome -> Navegador
+		Kdenlive -> Editor de videos
+		PinguyBuilder -> Remasterizador de sistema
+		SafeEyes -> Ferramenta de descanso dos olhos
+		Simple Screen Recorder -> Gravador de tela
+		Spotify -> Player de músicas
+		Transmageddon -> Conversor de videos
+		VirtualBox -> Virtualizador" 14 78
+}
+
+Detalhes
