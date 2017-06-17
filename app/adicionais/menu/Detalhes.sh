40c3d23af05b93b2a3f0eae1dbbd976509fb83b3
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-06-17 14:36:27 -0300

    Adicionando programas e descrição

--- app/adicionais/menu/Detalhes.sh
+++ app/adicionais/menu/Detalhes.sh
@@ -4,14 +4,16 @@ Detalhes() {
 	whiptail --title "Detalhes pacote adicionais" --msgbox "
 		Dropbox -> Serviço para armazenamento de arquivos
 		Franz -> Agregador de chats
+		Geogebra -> Programa que realiza cálculos e construção de gráficos
 		Google Chrome -> Navegador
 		Kdenlive -> Editor de videos
+		MasterPDF ->  Editor de PDF
 		PinguyBuilder -> Remasterizador de sistema
-		SafeEyes -> Ferramenta de descanso dos olhos
+		SafeEyes -> Alerta de descanso dos olhos
 		Simple Screen Recorder -> Gravador de tela
 		Spotify -> Player de músicas
 		Transmageddon -> Conversor de videos
-		VirtualBox -> Virtualizador" 14 78
+		VirtualBox -> Virtualizador" 18 78
 }
 
 Detalhes
