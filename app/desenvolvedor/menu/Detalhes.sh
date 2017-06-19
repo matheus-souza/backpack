b77bd8af751905dd50589b16884e44a03041cdcf
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-06-18 23:58:13 -0300

    Criando detalhes do pacote desenvolvedor

---
+++ app/desenvolvedor/menu/Detalhes.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+Detalhes() {
+	whiptail --title "Detalhes pacote básico" --msgbox "
+		AdminMongo ->  Interface Web para MongoDB
+		Android Studio -> IDE para Android
+		Curl -> Ferramenta para criar requisições em diversos protocolos
+		Docker -> Plataforma de administração de containers
+		Eclipse -> IDE multi-linguagem
+		GitKraken -> Cliente gráfico para git
+		Git -> Sistema de controle de versão
+		Java JDK -> Pacote de desenvolvimento Java
+		Meld -> Comparador de arquivos
+		MongoDB -> Banco de dados não relacional orientado a documentos
+		NetBeans -> IDE multi-linguagem
+		NodeJS -> Plataforma para desenvolvimento de aplicações server-side
+		Nodemon -> File watcher que roda internamente o comando node
+		PHP7.0 -> Linguagem de programação
+		PHP7.1 -> Linguagem de programação
+		PHP Storm -> IDE para PHP 
+		PgActivity -> Monitorador de servidor PostgreSQL
+		PgAdmin -> Software gráfico para administração do PostgreSQL
+		Portainer -> Gerenciador gráfico para Docker
+		PostgreSQL -> Banco de dados relacional
+		Postman -> Um cliente REST
+		PyCharm -> IDE para Python
+		SceneBuilder -> Criador de layout para JavaFX
+		Sublime Text -> Editor de texto e código-fonte 
+		Vagrant -> Ferramenta de criação de ambientes virtuai
+		WebStorm -> IDE para tecnologias web
+		Xampp -> Ambiente de desenvolvimento PHP
+		Yeoman -> Stack que automatiza seu build" 14 78
+}
+
+Detalhes
