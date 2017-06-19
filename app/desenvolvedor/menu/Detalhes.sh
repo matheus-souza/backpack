d6a42d6dddc4462a4188dc3ab7c98a503c191d38
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-06-19 00:19:10 -0300

    Commit de ajuste

--- app/desenvolvedor/menu/Detalhes.sh
+++ app/desenvolvedor/menu/Detalhes.sh
@@ -4,32 +4,32 @@ Detalhes() {
 	whiptail --title "Detalhes pacote básico" --msgbox "
 		AdminMongo ->  Interface Web para MongoDB
 		Android Studio -> IDE para Android
-		Curl -> Ferramenta para criar requisições em diversos protocolos
+		Curl -> Ferramenta para criar requisições
 		Docker -> Plataforma de administração de containers
 		Eclipse -> IDE multi-linguagem
-		GitKraken -> Cliente gráfico para git
+		GitKraken -> Cliente gráfico para Git
 		Git -> Sistema de controle de versão
 		Java JDK -> Pacote de desenvolvimento Java
 		Meld -> Comparador de arquivos
 		MongoDB -> Banco de dados não relacional orientado a documentos
 		NetBeans -> IDE multi-linguagem
-		NodeJS -> Plataforma para desenvolvimento de aplicações server-side
+		NodeJS -> Plataforma para desenvolvimento
 		Nodemon -> File watcher que roda internamente o comando node
 		PHP7.0 -> Linguagem de programação
 		PHP7.1 -> Linguagem de programação
-		PHP Storm -> IDE para PHP 
+		PHP Storm -> IDE para PHP
 		PgActivity -> Monitorador de servidor PostgreSQL
 		PgAdmin -> Software gráfico para administração do PostgreSQL
 		Portainer -> Gerenciador gráfico para Docker
 		PostgreSQL -> Banco de dados relacional
-		Postman -> Um cliente REST
+		Postman -> Cliente REST
 		PyCharm -> IDE para Python
 		SceneBuilder -> Criador de layout para JavaFX
-		Sublime Text -> Editor de texto e código-fonte 
-		Vagrant -> Ferramenta de criação de ambientes virtuai
+		Sublime Text -> Editor de texto e código-fonte
+		Vagrant -> Ferramenta de criação de ambientes virtuais
 		WebStorm -> IDE para tecnologias web
 		Xampp -> Ambiente de desenvolvimento PHP
-		Yeoman -> Stack que automatiza seu build" 14 78
+		Yeoman -> Stack que automatiza seu build" 36 78
 }
 
 Detalhes
