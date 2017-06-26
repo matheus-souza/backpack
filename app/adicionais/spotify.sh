254fa46d80842dc85a064171d974688d6424d76f
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-06-26 12:34:26 -0300

    Atualizando link para download do Spotify

--- app/adicionais/spotify.sh
+++ app/adicionais/spotify.sh
@@ -2,7 +2,7 @@
 
 usuario=$(./app/utils/UsuarioLogado.sh)
 
-wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.55.487.g256699aa-16_amd64.deb -O /home/$usuario/Downloads/spotify-client.deb
+wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.57.474.gca9c9538-30_amd64.deb -O /home/$usuario/Downloads/spotify-client.deb
 
 sudo dpkg -i /home/$usuario/Downloads/spotify-client.deb
 
