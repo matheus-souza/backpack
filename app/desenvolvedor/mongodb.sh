a41fca7008e46c6774e687075f3ea2a7c744ab8b
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-06-30 01:06:59 -0300

    Atualizando instalador do MongoDB

--- app/desenvolvedor/mongodb.sh
+++ app/desenvolvedor/mongodb.sh
@@ -2,7 +2,20 @@
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+InstalacaoTrusty() {
+	echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+}
+
+InstalacaoXenial() {
+	echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+}
+
+versaoUbuntu=$(./app/utils/VerificaVersaoUbuntu.sh -c)
+
+case $versaoUbuntu in
+	"trusty") InstalacaoTrusty ;;
+	"xenial") InstalacaoXenial ;;
+esac
 
 sudo apt-get update 
 
