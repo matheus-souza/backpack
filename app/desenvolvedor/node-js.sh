8386700d31f545186b1bb0860fc9dca8e3b30741
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-07-05 00:15:45 -0300

    Ajustando instalador do NodeJS

--- app/desenvolvedor/node-js.sh
+++ app/desenvolvedor/node-js.sh
@@ -2,13 +2,10 @@
 
 NodeJs() {
 	curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-	sudo apt-get install -y nodejs
+	
+	sudo apt-get install nodejs -y
 
-	sudo apt-get install -y build-essential
-
-	sudo apt-get install npm -y
-
-	sudo apt-get install nodejs-legacy -y
+	sudo apt-get install build-essential -y
 }
 
 Curl() {
