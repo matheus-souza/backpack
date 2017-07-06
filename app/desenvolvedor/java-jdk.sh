e3fb9d4360b0eb9875e438ead0265127281e8efe
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-07-05 22:58:40 -0300

    Ajustando script de instalação do Java JDK

--- app/desenvolvedor/java-jdk.sh
+++ app/desenvolvedor/java-jdk.sh
@@ -6,6 +6,9 @@ sudo add-apt-repository ppa:webupd8team/java -y
 
 sudo apt-get update
 
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+
 sudo apt-get install oracle-java8-installer -y
 
 sudo apt-get install oracle-java8-set-default -y
