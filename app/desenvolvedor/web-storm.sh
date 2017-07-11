44c9e3305901ac2394747cbf539bb107b73649f9
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-07-10 22:59:21 -0300

    Revert "Atualizando script de instalação do WebStorm"
    This reverts commit 8f414306fa2f4974064f8ba41de871c557f817be.

--- app/desenvolvedor/web-storm.sh
+++ app/desenvolvedor/web-storm.sh
@@ -2,18 +2,16 @@
 
 usuario=$(./app/utils/UsuarioLogado.sh)
 
-sudo rm /opt/WebStorm* -R
+wget https://download-cf.jetbrains.com/webstorm/WebStorm-2017.1.1.tar.gz -O /home/$usuario/Downloads/WebStorm-2017.1.1.tar.gz
 
-wget https://download-cf.jetbrains.com/webstorm/WebStorm-2017.1.4.tar.gz -O /home/$usuario/Downloads/WebStorm-2017.1.4.tar.gz
+sudo tar xf /home/$usuario/Downloads/WebStorm-2017.1.1.tar.gz -C /opt/
 
-sudo tar xf /home/$usuario/Downloads/WebStorm-2017.1.4.tar.gz -C /opt/
+sudo rm /home/$usuario/Downloads/WebStorm-2017.1.1.tar.gz
 
-sudo rm /home/$usuario/Downloads/WebStorm-2017.1.4.tar.gz
-
-cd /opt/WebStorm-171.4694.29/bin/
+cd /opt/WebStorm-171.4073.40/bin/
 
 sudo chmod +x ./webstorm.sh
 
-sudo chmod 777 /opt/WebStorm-171.4694.29 -R
+sudo chmod 777 /opt/WebStorm-171.4073.40 -R
 
 sudo -H -u $usuario bash -c "./webstorm.sh"
