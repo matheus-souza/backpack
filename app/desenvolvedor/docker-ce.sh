26b3006bd91d5fe93e5b9675c06f7df5ef9f8c39
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-07-12 12:57:40 -0300

    Ajustando script de instalação do Docker

--- app/desenvolvedor/docker-ce.sh
+++ app/desenvolvedor/docker-ce.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
+usuario=$(./app/utils/UsuarioLogado.sh)
+
 sudo apt-get remove docker docker-engine -y
 
 sudo apt-get update
 
 sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
 
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-
-sudo apt-get update
-
-sudo apt-get install docker-ce -y
-
 InstalacaoTrusty() {
+	wget -qO- https://get.docker.com/ | sh
+
+	sudo usermod -aG docker $usuario
+
 	sudo apt-get install python-pip -y
+
 	sudo pip install -U docker-compose
+	
 	sudo chmod +x /usr/local/bin/docker-compose
 }
 
 InstalacaoXenial() {
+	sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+
+	sudo apt-get update
+
+	sudo apt-get install docker-ce -y
+
 	sudo apt-get install docker-compose
 }
 
