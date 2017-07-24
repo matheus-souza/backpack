e415a2e0d63ea676fbac38f1571981998df1d23c
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-07-24 18:28:46 -0300

    Ajustando script

--- app/desenvolvedor/yeoman.sh
+++ app/desenvolvedor/yeoman.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-npm install -g yo
+Yeoman() {
+	sudo npm install -g yo
+}
+
+NodeJs() {
+	sudo ./app/desenvolvedor/node-js.sh
+}
+
+nodejs=$(./app/utils/VerificaInstalacaoNode.sh)
+
+if [[ $node != "true" ]]; then
+	NodeJs
+	Yeoman
+else
+	Yeoman
+fi
