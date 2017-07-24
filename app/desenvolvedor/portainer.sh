aa678000a9d54ff7c884c643b6546179f8653458
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-07-24 18:20:22 -0300

    Ajustando script

--- app/desenvolvedor/portainer.sh
+++ app/desenvolvedor/portainer.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 usuario=$(./app/utils/UsuarioLogado.sh)
+Portainer() {
+	if [[ ! -d "/home/$usuario/portainer/docker-compose.yml" ]]; then
+		mkdir /home/$usuario/docker
 
-if [[ ! -d "/home/$usuario/docker/docker-compose.yml" ]]; then
-	mkdir /home/$usuario/docker
+		wget https://gist.githubusercontent.com/matheus-souza/a888eea8902159f690b53aae2005343b/raw/346ac1bff8e9559c4b84abbaaef4378e2c2a4484/docker-compose.yml -O /home/$usuario/docker/docker-compose.yml
+	fi
 
-	wget https://gist.githubusercontent.com/matheus-souza/a888eea8902159f690b53aae2005343b/raw/346ac1bff8e9559c4b84abbaaef4378e2c2a4484/docker-compose.yml -O /home/$usuario/docker/docker-compose.yml
+	chmod 777 /home/$usuario/docker -R
+
+	cd /home/$usuario/docker/
+
+	sudo docker-compose up -d	
+}
+
+Docker() {
+	sudo ./app/desenvolvedor/docker-ce.sh
+}
+
+docker=$(./app/utils/VerificaInstalacaoDocker.sh)
+
+if [[ $docker != "true" ]]; then
+	Docker
+	Portainer
+else
+	Portainer
 fi
-
-InstalacaoTrusty() {
-	sudo apt-get install python-pip -y
-	sudo pip install -U docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
-}
-
-InstalacaoXenial() {
-	sudo apt-get install docker-compose
-}
-
-case $(lsb_release -cs) in
-	"trusty") InstalacaoTrusty ;;
-	"xenial") InstalacaoXenial ;;
-esac
-
-chmod 777 /home/$usuario/docker -R
-
-cd /home/$usuario/docker/
-
-sudo docker-compose up -d
