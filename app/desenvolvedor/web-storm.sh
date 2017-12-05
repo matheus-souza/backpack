239de51fc8dcdf61ed6990d0585460ad0dc7aa1d
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-12-05 08:50:00 -0200

    Update web-storm.sh

--- app/desenvolvedor/web-storm.sh
+++ app/desenvolvedor/web-storm.sh
@@ -4,16 +4,16 @@ usuario=$(./app/utils/UsuarioLogado.sh)
 
 sudo rm /opt/WebStorm* -R
 
-wget https://download-cf.jetbrains.com/webstorm/WebStorm-2017.1.4.tar.gz -O /home/$usuario/Downloads/WebStorm-2017.1.4.tar.gz
+wget https://download-cf.jetbrains.com/webstorm/WebStorm-2017.3.tar.gz -O /home/$usuario/Downloads/WebStorm.tar.gz
 
-sudo tar xf /home/$usuario/Downloads/WebStorm-2017.1.4.tar.gz -C /opt/
+sudo tar xf /home/$usuario/Downloads/WebStorm.tar.gz -C /opt/
 
-sudo rm /home/$usuario/Downloads/WebStorm-2017.1.4.tar.gz
+sudo rm /home/$usuario/Downloads/WebStorm.tar.gz
 
-cd /opt/WebStorm-171.4694.29/bin/
+cd /opt/WebStorm*/bin/
 
 sudo chmod +x ./webstorm.sh
 
-sudo chmod 777 /opt/WebStorm-171.4694.29 -R
+sudo chmod 777 /opt/WebStorm* -R
 
 sudo -H -u $usuario bash -c "./webstorm.sh"
