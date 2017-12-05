eb912f6b4e0313c7f64e6db286617c8541e95b28
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-12-05 08:40:00 -0200

    Update spotify.sh

--- app/adicionais/spotify.sh
+++ app/adicionais/spotify.sh
@@ -2,7 +2,7 @@
 
 usuario=$(./app/utils/UsuarioLogado.sh)
 
-wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.64.407.g9bd02c2d-26_amd64.deb -O /home/$usuario/Downloads/spotify-client.deb
+wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.67.582.g19436fa3-28_amd64.deb -O /home/$usuario/Downloads/spotify-client.deb
 
 sudo dpkg -i /home/$usuario/Downloads/spotify-client.deb
 
