ba48ae41576bb5348a664822ae7a5cbdee1b3f63
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-12-20 13:40:51 -0200

    Atualizado versão do Spotify

--- app/adicionais/spotify.sh
+++ app/adicionais/spotify.sh
@@ -2,7 +2,7 @@
 
 usuario=$(./app/utils/UsuarioLogado.sh)
 
-wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.67.582.g19436fa3-28_amd64.deb -O /home/$usuario/Downloads/spotify-client.deb
+wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.0.69.336.g7edcc575-39_amd64.deb -O /home/$usuario/Downloads/spotify-client.deb
 
 sudo dpkg -i /home/$usuario/Downloads/spotify-client.deb
 
