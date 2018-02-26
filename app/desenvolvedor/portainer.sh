235f6a5feb25ddb95e34f18e2355f738b36a5b60
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-02-26 18:52:43 -0300

    Update Portainer

--- app/desenvolvedor/portainer.sh
+++ app/desenvolvedor/portainer.sh
@@ -3,16 +3,16 @@
 usuario=$(./app/utils/UsuarioLogado.sh)
 Portainer() {
 	if [[ ! -d "/home/$usuario/portainer/docker-compose.yml" ]]; then
-		mkdir /home/$usuario/docker
+		mkdir /home/$usuario/docker/portainer
 
-		wget https://gist.githubusercontent.com/matheus-souza/a888eea8902159f690b53aae2005343b/raw/346ac1bff8e9559c4b84abbaaef4378e2c2a4484/docker-compose.yml -O /home/$usuario/docker/docker-compose.yml
+		wget https://gist.githubusercontent.com/matheus-souza/a888eea8902159f690b53aae2005343b/raw/346ac1bff8e9559c4b84abbaaef4378e2c2a4484/docker-compose.yml -O /home/$usuario/docker/portainer/docker-compose.yml
 	fi
 
 	chmod 777 /home/$usuario/docker -R
 
 	cd /home/$usuario/docker/
 
-	sudo docker-compose up -d	
+	docker-compose up -d
 }
 
 Docker() {
